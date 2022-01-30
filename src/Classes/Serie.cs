2c629ffcb21bfb56e67c6c6509b7fd9ac5b5381a
Davi <davicamiloribeiro@gmail.com>
2022-01-30 18:37:58 -0300

    Ajustes de Classe Serie e SerieRepositorio, metodos no Program

--- src/Classes/Serie.cs
+++ src/Classes/Serie.cs
@@ -1,4 +1,4 @@
-﻿using CadastroSeries.src.Enum;
+﻿using CadastroSeries;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +13,7 @@ namespace CadastroSeries
         private string Titulo { get; set; }
         private string Descricao { get; set;}
         private int Ano { get; set; }
+        private bool Excluido { get; set; }
 
         public Serie(int id, Genero genero, string titulo, string descricao, int ano) 
         {
@@ -21,6 +22,7 @@ namespace CadastroSeries
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
         }
 
         public override string ToString()
@@ -41,6 +43,16 @@ namespace CadastroSeries
         public int retornaId() 
         {
             return this.Id;
+        }
+
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
         }
     }
 }
--- src/Enum/Genero.cs
+++ src/Enum/Genero.cs
@@ -1,4 +1,4 @@
-﻿namespace CadastroSeries.src.Enum
+﻿namespace CadastroSeries
 {
     public enum Genero 
     { 
