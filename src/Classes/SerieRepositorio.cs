94fa42178166d6236711a7c3194a07b98486afb9
Davi <davicamiloribeiro@gmail.com>
2022-01-29 22:45:50 -0300

    Adição de Interface

---
+++ src/Classes/SerieRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CadastroSeries.src.Interfaces;
+
+namespace CadastroSeries
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        public void Atualiza(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
